9f01e92545e1eaba0ec2d1ab5900bbcd5c8b1278
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-11-29 16:12:19 +0200

    Create new Todo from user data

---
+++ controllers/todos.js
@@ -0,0 +1,18 @@
+import { Todo } from "../models/todo.js";
+
+class todoController {
+  constructor() {
+    this.TODOS = [];
+  }
+  createTodo(req, res) {
+    const task = req.body.task;
+    const newTodo = new Todo(Math.random().toString(), task);
+    this.TODOS.push(newTodo);
+    res.json({
+      message: "created new todo object",
+      newTask: newTodo,
+    });
+  }
+}
+
+export const TodoController = new todoController();
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import todoRoutes from "./routes/todos.js";
+
 const app = express();
 app.use(bodyParser.json());
+
+app.use(express.urlencoded({ urlencoded: true }));
+
+app.use("/todos", todoRoutes);
 
 app.get("/json-test", (req, res) => {
   res.send({
